a8fd1582ffc6ec862aac81f1516770aed0f50478
thefivetoes <justin.smith@thundertech.com>
2014-05-19 22:30:00 -0400

    initial dev

---
+++ app/controllers/accounts/transaction.js
@@ -0,0 +1,24 @@
+export default Ember.ArrayController.extend({
+  itemController: 'Accounts/TransactionItem',
+  currentEditingItem: null,
+  actions:{
+    startEditing: function(item){
+      var currentItem = this.get('currentEditingItem');
+      if( currentItem !== item ){
+        if( currentItem ) {
+          currentItem.set( 'isEditing', false );
+        }
+        item.set( 'isEditing', true );
+        this.set( 'currentEditingItem', item );
+      }
+    }
+  },
+  stopEditing: function(){
+    var currentItem = this.get('currentEditingItem');
+    currentItem.set( 'isEditing', false );
+    if( currentItem.get( 'isDirty' ) ){
+      currentItem.get( 'content' ).save();
+    }
+    this.set( 'currentEditingItem', null );
+  }
+});
--- app/router.js
+++ app/router.js
@@ -1,11 +1,13 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('component-test');
-  this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+  //this.route('component-test');
+  //this.route('helper-test');
+  this.resource( 'accounts', function(){
+    this.resource( 'account', {path: ':account_id'}, function(){
+      this.resource( 'accounts.transactions', {path: 'transactions'} );
+    } );
+  } );
 });
 
 export default Router;
